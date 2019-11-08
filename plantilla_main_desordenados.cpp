8744768a9d10a5004fe9711e5614f1ef6ea6bfe2
manuacostaf18 <55296684+manuacostaf18@users.noreply.github.com>
2019-11-08 15:14:21 -0500

    Update plantilla_main_desordenados.cpp

--- plantilla_main_desordenados.cpp
+++ plantilla_main_desordenados.cpp
@@ -3,16 +3,13 @@
 #include <time.h>
 using namespace std;
 
-int main()
-{
+int main(){
     srand(time(NULL));
     
-    for(int i = 1; i < 11; i++)
-    {
+    for(int i = 1; i < 11; i++){
         int num = 1 + rand() % (11 - 1);
         cout << num<< " ";
     }
     
     return 0;
-    
 }
