2035fd31f4c7b4131f725aaf6cf2c7e696ea45a9
manuacostaf18 <55296684+manuacostaf18@users.noreply.github.com>
2019-11-09 09:37:04 -0500

    Update plantilla_main_ordenados.cpp

--- plantilla_main_ordenados.cpp
+++ plantilla_main_ordenados.cpp
@@ -4,16 +4,22 @@
 #include <sys/time.h>
 using namespace std;
 
+//Funciones adicionales
 double gettime(){
   struct timeval tp;
   gettimeofday(&tp, NULL);
   return tp.tv_sec + tp.tv_usec/(double)1.0e6;
 }
 
+void swap(int i, int j, Vector& vector){
+  int temp = vector.get(i);
+  vector.set(i, vector.get(j));
+  vector.set(j, temp);
+}
+
+//Algoritmo
 /*
-
 PONER ACÁ EL CÓDIGO DEL ALGORITMO CORRESPONDIENTE
-
 */
 
 int main(){
@@ -42,49 +48,49 @@ int main(){
 	vector4_ordenado.display();
 	cout << endl << "Vector 5 ordenado: " << endl;
 	vector5_ordenado.display();*/
-	
+
 	//Probando el algoritmo con cada uno de los 5 vectores ordenados. 
 	double t1_0 = gettime();
-	Vector vector1_ordenado_end = //Poner algoritmo acá, con los argumentos necesarios, para ordenar el vector1_ordenado.
+	//Algoritmo con vector 1.
 	double t1_1 = gettime();
 	double t1 = t1_1 - t1_0;
 	cout << "Tiempo tomado ordenando al vector1_ordenado = " << t1 << endl;
 	
 	double t2_0 = gettime();
-	Vector vector2_ordenado_end = //Poner algoritmo acá, con los argumentos necesarios, para ordenar el vector2_ordenado.
+	//Algoritmo con vector 2.
 	double t2_1 = gettime();
 	double t2 = t2_1 - t2_0;
 	cout << "Tiempo tomado ordenando al vector2_ordenado = " << t2 << endl;
 	
 	double t3_0 = gettime();
-	Vector vector3_ordenado_end = //Poner algoritmo acá, con los argumentos necesarios, para ordenar el vector3_ordenado.
+	//Algoritmo con vector 3.
 	double t3_1 = gettime();
 	double t3 = t3_1 - t3_0;
 	cout << "Tiempo tomado ordenando al vector3_ordenado = " << t3 << endl;
 	
 	double t4_0 = gettime();
-	Vector vector4_ordenado_end = //Poner algoritmo acá, con los argumentos necesarios, para ordenar el vector4_ordenado.
+	//Algoritmo con vector 4.
 	double t4_1 = gettime();
 	double t4 = t4_1 - t4_0;
 	cout << "Tiempo tomado ordenando al vector4_ordenado = " << t4 << endl;
 	
 	double t5_0 = gettime();
-	Vector vector5_ordenado_end = //Poner algoritmo acá, con los argumentos necesarios, para ordenar el vector5_ordenado.
+	//Algoritmo con vector 5.
 	double t5_1 = gettime();
 	double t5 = t5_1 - t5_0;
 	cout << "Tiempo tomado ordenando al vector5_ordenado = " << t5 << endl;
 	
 	//Imprimiendo los vectores ordenados, después del algoritmo (opcional)
 	/*cout << "Vector 1 ordenado: " << endl;
-	vector1_ordenado_end.display();
+	vector1_ordenado.display();
 	cout << endl << "Vector 2 ordenado: " << endl;
-	vector2_ordenado_end.display();
+	vector2_ordenado.display();
 	cout << endl << "Vector 3 ordenado: " << endl;
-	vector3_ordenado_end.display();
+	vector3_ordenado.display();
 	cout << endl << "Vector 4 ordenado: " << endl;
-	vector4_ordenado_end.display();
+	vector4_ordenado.display();
 	cout << endl << "Vector 5 ordenado: " << endl;
-	vector5_ordenado_end.display();*/
+	vector5_ordenado.display();*/
 
 	return 0;
 	
