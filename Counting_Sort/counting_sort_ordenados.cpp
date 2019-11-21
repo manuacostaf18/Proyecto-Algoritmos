02d3df7aaa9619e1c2a85493427b6fb5670c1016
mariagaviria <44762498+mariagaviria@users.noreply.github.com>
2019-11-20 19:42:30 -0500

    Add files via upload

---
+++ Counting_Sort/counting_sort_ordenados.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include "vec.hpp"
+#include <set>
+#include <sys/time.h>
+#include<map>
+using namespace std;
+
+//Funciones adicionales
+double gettime(){
+  struct timeval tp;
+  gettimeofday(&tp, NULL);
+  return tp.tv_sec + tp.tv_usec/(double)1.0e6;
+}
+
+void swap(int i, int j, Vec& vector){
+  int temp = vector.get(i);
+  vector.set(i, vector.get(j));
+  vector.set(j, temp);
+}
+
+//Algoritmo
+void countSort(Vec& vector){
+  map<int, int> freq;
+
+  for(int i = 0; i < vector.getsize(); i++){
+    freq[vector.get(i)]++;
+  }
+  int i = 0;
+  for(auto it: freq){
+    while(it.second--){
+      vector.set(i++, it.first);
+    }
+  }
+}
+
+int main(){
+
+	//Creando los 5 vectores ordenados, de tamaños diferentes
+	Vec vector1_ordenado;
+	Vec vector2_ordenado;
+	Vec vector3_ordenado;
+	Vec vector4_ordenado;
+	Vec vector5_ordenado;
+
+	for(int i = 1; i < 6; i++) vector1_ordenado.push_back(i);
+	for(int i = 1; i < 11; i++) vector2_ordenado.push_back(i);
+	for(int i = 1; i < 51; i++) vector3_ordenado.push_back(i);
+	for(int i = 1; i < 101; i++) vector4_ordenado.push_back(i);
+	for(int i = 1; i < 1001; i++) vector5_ordenado.push_back(i);
+
+	//Imprimiendo los vectores ordenados (opcional)
+	/*cout << "Vector 1 ordenado: " << endl;
+	vector1_ordenado.display();
+	cout << endl << "Vector 2 ordenado: " << endl;
+	vector2_ordenado.display();
+	cout << endl << "Vector 3 ordenado: " << endl;
+	vector3_ordenado.display();
+	cout << endl << "Vector 4 ordenado: " << endl;
+	vector4_ordenado.display();
+	cout << endl << "Vector 5 ordenado: " << endl;
+	vector5_ordenado.display();*/
+
+	//Probando el algoritmo con cada uno de los 5 vectores ordenados.
+	double t1_0 = gettime();
+  countSort(vector1_ordenado);
+	double t1_1 = gettime();
+	double t1 = t1_1 - t1_0;
+	cout << "Tiempo tomado ordenando al vector1_ordenado = " << t1 << endl;
+
+	double t2_0 = gettime();
+  countSort(vector2_ordenado);
+	double t2_1 = gettime();
+	double t2 = t2_1 - t2_0;
+	cout << "Tiempo tomado ordenando al vector2_ordenado = " << t2 << endl;
+
+	double t3_0 = gettime();
+  countSort(vector3_ordenado);
+	double t3_1 = gettime();
+	double t3 = t3_1 - t3_0;
+	cout << "Tiempo tomado ordenando al vector3_ordenado = " << t3 << endl;
+
+	double t4_0 = gettime();
+  countSort(vector4_ordenado);
+	double t4_1 = gettime();
+	double t4 = t4_1 - t4_0;
+	cout << "Tiempo tomado ordenando al vector4_ordenado = " << t4 << endl;
+
+	double t5_0 = gettime();
+  countSort(vector5_ordenado);
+	double t5_1 = gettime();
+	double t5 = t5_1 - t5_0;
+	cout << "Tiempo tomado ordenando al vector5_ordenado = " << t5 << endl;
+
+	//Imprimiendo los vectores ordenados, después del algoritmo (opcional)
+	/*cout << "Vector 1 ordenado: " << endl;
+	vector1_ordenado.display();
+	cout << endl << "Vector 2 ordenado: " << endl;
+	vector2_ordenado.display();
+	cout << endl << "Vector 3 ordenado: " << endl;
+	vector3_ordenado.display();
+	cout << endl << "Vector 4 ordenado: " << endl;
+	vector4_ordenado.display();
+	cout << endl << "Vector 5 ordenado: " << endl;
+	vector5_ordenado.display();*/
+
+	return 0;
+
+}
