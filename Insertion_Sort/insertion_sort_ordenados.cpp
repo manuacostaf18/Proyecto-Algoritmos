82e176edcb31e5b082ff180177392259a4e97259
mariagaviria <44762498+mariagaviria@users.noreply.github.com>
2019-11-23 23:14:45 -0500

    Add files via upload

--- Insertion_Sort/insertion_sort_ordenados.cpp
+++ Insertion_Sort/insertion_sort_ordenados.cpp
@@ -11,88 +11,89 @@ double gettime(){
   return tp.tv_sec + tp.tv_usec/(double)1.0e6;
 }
 
-void swap(int i, int j, Vector& vector){
+void swap(int i, int j, Vec& vector){
   int temp = vector.get(i);
   vector.set(i, vector.get(j));
   vector.set(j, temp);
 }
 
 //Algoritmo
-void insertion_sort(Vector& vector){
+void insertion_sort(Vec& vector){
 	for(int i = 1; i < vector.getsize(); ++i){
 		int a = i;
 		int b = i-1;
 		while(vector.get(b) > vector.get(a)){
 			swap(a, b, vector);
+      a--;
 			b = a-1;
 		}
 	}
 }
 
 int main(){
-	
-	//Creando los 5 vectores ordenados, de tamaños diferentes 
-	Vector vector1_ordenado;
-	Vector vector2_ordenado;
-	Vector vector3_ordenado;
-	Vector vector4_ordenado;
-	Vector vector5_ordenado;
-	
+
+	//Creando los 5 vectores ordenados, de tamaños diferentes
+	Vec vector1_ordenado;
+	Vec vector2_ordenado;
+	Vec vector3_ordenado;
+	Vec vector4_ordenado;
+	Vec vector5_ordenado;
+
 	for(int i = 1; i < 6; i++) vector1_ordenado.push_back(i);
 	for(int i = 1; i < 11; i++) vector2_ordenado.push_back(i);
 	for(int i = 1; i < 51; i++) vector3_ordenado.push_back(i);
 	for(int i = 1; i < 101; i++) vector4_ordenado.push_back(i);
 	for(int i = 1; i < 1001; i++) vector5_ordenado.push_back(i);
-	
+
 	//Imprimiendo los vectores ordenados (opcional)
-	/*cout << "Vector 1 ordenado: " << endl;
+	cout << "Vector 1 ordenado: " << endl;
 	vector1_ordenado.display();
 	cout << endl << "Vector 2 ordenado: " << endl;
 	vector2_ordenado.display();
-	cout << endl << "Vector 3 ordenado: " << endl;
+	/*cout << endl << "Vector 3 ordenado: " << endl;
 	vector3_ordenado.display();
 	cout << endl << "Vector 4 ordenado: " << endl;
 	vector4_ordenado.display();
 	cout << endl << "Vector 5 ordenado: " << endl;
 	vector5_ordenado.display();*/
 
-	//Probando el algoritmo con cada uno de los 5 vectores ordenados. 
+	//Probando el algoritmo con cada uno de los 5 vectores ordenados.
 	double t1_0 = gettime();
 	insertion_sort(vector1_ordenado);
 	double t1_1 = gettime();
 	double t1 = t1_1 - t1_0;
 	cout << "Tiempo tomado ordenando al vector1_ordenado = " << t1 << endl;
-	
+
 	double t2_0 = gettime();
 	insertion_sort(vector2_ordenado);
 	double t2_1 = gettime();
 	double t2 = t2_1 - t2_0;
 	cout << "Tiempo tomado ordenando al vector2_ordenado = " << t2 << endl;
-	
+
 	double t3_0 = gettime();
 	insertion_sort(vector3_ordenado);
 	double t3_1 = gettime();
 	double t3 = t3_1 - t3_0;
 	cout << "Tiempo tomado ordenando al vector3_ordenado = " << t3 << endl;
-	
+
 	double t4_0 = gettime();
 	insertion_sort(vector4_ordenado);
 	double t4_1 = gettime();
 	double t4 = t4_1 - t4_0;
 	cout << "Tiempo tomado ordenando al vector4_ordenado = " << t4 << endl;
-	
+
 	double t5_0 = gettime();
 	insertion_sort(vector5_ordenado);
 	double t5_1 = gettime();
 	double t5 = t5_1 - t5_0;
 	cout << "Tiempo tomado ordenando al vector5_ordenado = " << t5 << endl;
-	
+
 	//Imprimiendo los vectores ordenados, después del algoritmo (opcional)
-	/*cout << "Vector 1 ordenado: " << endl;
+	cout << "Vector 1 ordenado: " << endl;
 	vector1_ordenado.display();
 	cout << endl << "Vector 2 ordenado: " << endl;
 	vector2_ordenado.display();
-	cout << endl << "Vector 3 ordenado: " << endl;
+	/*cout << endl << "Vector 3 ordenado: " << endl;
 	vector3_ordenado.display();
 	cout << endl << "Vector 4 ordenado: " << endl;
 	vector4_ordenado.display();
@@ -100,5 +101,5 @@ int main(){
 	vector5_ordenado.display();*/
 
 	return 0;
-	
+
 }
