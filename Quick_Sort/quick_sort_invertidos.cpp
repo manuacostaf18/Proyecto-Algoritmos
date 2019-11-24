a8a6429cad62fbb97e31b0fe948267a29a00ee0a
mariagaviria <44762498+mariagaviria@users.noreply.github.com>
2019-11-24 00:41:07 -0500

    Add files via upload

--- Quick_Sort/quick_sort_invertidos.cpp
+++ Quick_Sort/quick_sort_invertidos.cpp
@@ -21,55 +21,55 @@ void swap(int i, int j, Vec& vector){
 int partition(Vec & vector, int low, int high){
 	int pivot = vector.get(high);
 	int i = (low -1);
-	
+
 	for(int j = low; j <= high - 1; j++){
 		if(vector.get(j) < pivot){
 			i++;
-			swap(vector.get(i), vector.get(j), vector);
+			swap(i, j, vector);
 		}
 	}
-	
-	swap(vector.get(i+1), vector.get(high), vector);
+
+	swap(i+1, high, vector);
 	return (i+1);
 }
 
 void quicksort(Vec & vector, int low, int high){
 	if(low < high){
 		int pi = partition(vector, low, high);
-		
+
 		quicksort(vector, low, pi - 1);
 		quicksort(vector, pi + 1, high);
 	}
 }
 
 int main(){
-	
+
 	//Creando los 5 vectores ordenados pero invertidos, de tamaños diferentes
 	Vec vector1_invertido;
 	Vec vector2_invertido;
 	Vec vector3_invertido;
 	Vec vector4_invertido;
 	Vec vector5_invertido;
-	
+
 	for(int i = 5; i > 0; i--) vector1_invertido.push_back(i);
 	for(int i = 10; i > 0; i--) vector2_invertido.push_back(i);
 	for(int i = 50; i > 0; i--) vector3_invertido.push_back(i);
 	for(int i = 100; i > 0; i--) vector4_invertido.push_back(i);
 	for(int i = 1000; i > 0; i--) vector5_invertido.push_back(i);
-	
+
 	//Imprimiendo los vectores invertidos (opcional)
-	/*cout << endl << "Vector 1 invertido: " << endl;
+	cout << endl << "Vector 1 invertido: " << endl;
 	vector1_invertido.display();
 	cout << endl << "Vector 2 invertido: " << endl;
 	vector2_invertido.display();
-	cout << endl << "Vector 3 invertido: " << endl;
+	/*cout << endl << "Vector 3 invertido: " << endl;
 	vector3_invertido.display();
 	cout <<  endl << "Vector 4 invertido: " << endl;
 	vector4_invertido.display();
 	cout << endl << "Vector 5 invertido: " << endl;
 	vector5_invertido.display();*/
-	
-	//Probando el algoritmo con cada uno de los 5 vectores invertidos. 
+
+	//Probando el algoritmo con cada uno de los 5 vectores invertidos.
 	double t1_0 = gettime();
 	int L1 = 0;
 	int R1 = vector1_invertido.getsize()-1;
@@ -77,7 +77,7 @@ int main(){
 	double t1_1 = gettime();
 	double t1 = t1_1 - t1_0;
 	cout << "Tiempo tomado ordenando al vector1_invertido = " << t1 << endl;
-	
+
 	double t2_0 = gettime();
 	int L2 = 0;
 	int R2 = vector2_invertido.getsize()-1;
@@ -85,7 +85,7 @@ int main(){
 	double t2_1 = gettime();
 	double t2 = t2_1 - t2_0;
 	cout << "Tiempo tomado ordenando al vector2_invertido = " << t2 << endl;
-	
+
 	double t3_0 = gettime();
 	int L3 = 0;
 	int R3 = vector3_invertido.getsize()-1;
@@ -93,7 +93,7 @@ int main(){
 	double t3_1 = gettime();
 	double t3 = t3_1 - t3_0;
 	cout << "Tiempo tomado ordenando al vector3_invertido = " << t3 << endl;
-	
+
 	double t4_0 = gettime();
 	int L4 = 0;
 	int R4 = vector4_invertido.getsize()-1;
@@ -101,7 +101,7 @@ int main(){
 	double t4_1 = gettime();
 	double t4 = t4_1 - t4_0;
 	cout << "Tiempo tomado ordenando al vector4_invertido = " << t4 << endl;
-	
+
 	double t5_0 = gettime();
 	int L5 = 0;
 	int R5 = vector5_invertido.getsize()-1;
@@ -109,13 +109,13 @@ int main(){
 	double t5_1 = gettime();
 	double t5 = t5_1 - t5_0;
 	cout << "Tiempo tomado ordenando al vector5_invertido = " << t5 << endl;
-	
+
 	//Imprimiendo los vectores invertidos, ya ordenados, después del algoritmo (opcional)
-	/*cout << endl << "Vector 1 invertido ya ordenado " << endl;
+	cout << endl << "Vector 1 invertido ya ordenado " << endl;
 	vector1_invertido.display();
 	cout << endl << "Vector 2 invertido ya ordenado: " << endl;
 	vector2_invertido.display();
-	cout << endl << "Vector 3 invertido ya ordenado: " << endl;
+	/*cout << endl << "Vector 3 invertido ya ordenado: " << endl;
 	vector3_invertido.display();
 	cout << endl << "Vector 4 invertido ya ordenado: " << endl;
 	vector4_invertido.display();
@@ -124,5 +124,5 @@ int main(){
 
 
 	return 0;
-	
+
 }
--- Quick_Sort/quick_sort_ordenados.cpp
+++ Quick_Sort/quick_sort_ordenados.cpp
@@ -21,55 +21,55 @@ void swap(int i, int j, Vec& vector){
 int partition(Vec & vector, int low, int high){
 	int pivot = vector.get(high);
 	int i = (low -1);
-	
+
 	for(int j = low; j <= high - 1; j++){
 		if(vector.get(j) < pivot){
 			i++;
-			swap(vector.get(i), vector.get(j), vector);
+			swap(i, j, vector);
 		}
 	}
-	
-	swap(vector.get(i+1), vector.get(high), vector);
+
+	swap(i+1, high, vector);
 	return (i+1);
 }
 
 void quicksort(Vec & vector, int low, int high){
 	if(low < high){
 		int pi = partition(vector, low, high);
-		
+
 		quicksort(vector, low, pi - 1);
 		quicksort(vector, pi + 1, high);
 	}
 }
 
 int main(){
-	
-	//Creando los 5 vectores ordenados, de tamaños diferentes 
+
+	//Creando los 5 vectores ordenados, de tamaños diferentes
 	Vec vector1_ordenado;
 	Vec vector2_ordenado;
 	Vec vector3_ordenado;
 	Vec vector4_ordenado;
 	Vec vector5_ordenado;
-	
+
 	for(int i = 1; i < 6; i++) vector1_ordenado.push_back(i);
 	for(int i = 1; i < 11; i++) vector2_ordenado.push_back(i);
 	for(int i = 1; i < 51; i++) vector3_ordenado.push_back(i);
 	for(int i = 1; i < 101; i++) vector4_ordenado.push_back(i);
 	for(int i = 1; i < 1001; i++) vector5_ordenado.push_back(i);
-	
+
 	//Imprimiendo los vectores ordenados (opcional)
-	/*cout << "Vector 1 ordenado: " << endl;
+	cout << "Vector 1 ordenado: " << endl;
 	vector1_ordenado.display();
 	cout << endl << "Vector 2 ordenado: " << endl;
 	vector2_ordenado.display();
-	cout << endl << "Vector 3 ordenado: " << endl;
+	/*cout << endl << "Vector 3 ordenado: " << endl;
 	vector3_ordenado.display();
 	cout << endl << "Vector 4 ordenado: " << endl;
 	vector4_ordenado.display();
 	cout << endl << "Vector 5 ordenado: " << endl;
 	vector5_ordenado.display();*/
 
-	//Probando el algoritmo con cada uno de los 5 vectores ordenados. 
+	//Probando el algoritmo con cada uno de los 5 vectores ordenados.
 	double t1_0 = gettime();
 	int L1 = 0;
 	int R1 = vector1_ordenado.getsize()-1;
@@ -77,7 +77,7 @@ int main(){
 	double t1_1 = gettime();
 	double t1 = t1_1 - t1_0;
 	cout << "Tiempo tomado ordenando al vector1_ordenado = " << t1 << endl;
-	
+
 	double t2_0 = gettime();
 	int L2 = 0;
 	int R2 = vector2_ordenado.getsize()-1;
@@ -85,7 +85,7 @@ int main(){
 	double t2_1 = gettime();
 	double t2 = t2_1 - t2_0;
 	cout << "Tiempo tomado ordenando al vector2_ordenado = " << t2 << endl;
-	
+
 	double t3_0 = gettime();
 	int L3 = 0;
 	int R3 = vector3_ordenado.getsize()-1;
@@ -93,7 +93,7 @@ int main(){
 	double t3_1 = gettime();
 	double t3 = t3_1 - t3_0;
 	cout << "Tiempo tomado ordenando al vector3_ordenado = " << t3 << endl;
-	
+
 	double t4_0 = gettime();
 	int L4 = 0;
 	int R4 = vector4_ordenado.getsize()-1;
@@ -101,7 +101,7 @@ int main(){
 	double t4_1 = gettime();
 	double t4 = t4_1 - t4_0;
 	cout << "Tiempo tomado ordenando al vector4_ordenado = " << t4 << endl;
-	
+
 	double t5_0 = gettime();
 	int L5 = 0;
 	int R5 = vector5_ordenado.getsize()-1;
@@ -109,13 +109,13 @@ int main(){
 	double t5_1 = gettime();
 	double t5 = t5_1 - t5_0;
 	cout << "Tiempo tomado ordenando al vector5_ordenado = " << t5 << endl;
-	
+
 	//Imprimiendo los vectores ordenados, después del algoritmo (opcional)
-	/*cout << "Vector 1 ordenado: " << endl;
+	cout << "Vector 1 ordenado: " << endl;
 	vector1_ordenado.display();
 	cout << endl << "Vector 2 ordenado: " << endl;
 	vector2_ordenado.display();
-	cout << endl << "Vector 3 ordenado: " << endl;
+	/*cout << endl << "Vector 3 ordenado: " << endl;
 	vector3_ordenado.display();
 	cout << endl << "Vector 4 ordenado: " << endl;
 	vector4_ordenado.display();
@@ -123,5 +123,5 @@ int main(){
 	vector5_ordenado.display();*/
 
 	return 0;
-	
+
 }
